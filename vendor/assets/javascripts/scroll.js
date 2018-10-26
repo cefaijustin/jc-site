4eb39849a2e80dbd96279c2be2628923a9b7f4b5
Justin Cefai <cefaijustin@gmail.com>
2018-10-26 22:14:56 +0000

    blog 4 added

--- vendor/assets/javascripts/scroll.js
+++ vendor/assets/javascripts/scroll.js
@@ -34,3 +34,15 @@ $('a[href*="#"]')
       }
     }
   });
+
+  $(".down-arrow a[href^='#web-apps']").on('click', function(event) {
+  var target = this.hash;
+
+  event.preventDefault();
+
+  return $('html, body').animate({
+    scrollTop: $(this.hash).offset().top - navOffset
+  }, 300, function() {
+    return window.history.pushState(null, null, target);
+  });
+});
