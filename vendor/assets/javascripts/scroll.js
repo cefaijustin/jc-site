039dea8f725f7a5d6c2cf2ae7d5fe54fd3cd6d90
Justin Cefai <cefaijustin@gmail.com>
2018-11-03 20:30:08 +0000

    logo href removed

--- vendor/assets/javascripts/scroll.js
+++ vendor/assets/javascripts/scroll.js
@@ -35,14 +35,6 @@ $('a[href*="#"]')
     }
   });
 
-  $(".down-arrow a[href^='#web-apps']").on('click', function(event) {
-  var target = this.hash;
+  
 
-  event.preventDefault();
-
-  return $('html, body').animate({
-    scrollTop: $(this.hash).offset().top - navOffset
-  }, 300, function() {
-    return window.history.pushState(null, null, target);
-  });
-});
+  
