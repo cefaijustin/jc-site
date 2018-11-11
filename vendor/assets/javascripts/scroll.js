a504230e20f7cadacf657268f9acdc52c6270eb5
cefaijustin <cefaijustin@gmail.com>
2018-11-11 01:49:35 +0000

    smooth scroll to top

--- vendor/assets/javascripts/scroll.js
+++ vendor/assets/javascripts/scroll.js
@@ -1,8 +1,6 @@
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
   .click(function(event) {
     // On-page links
     if (
