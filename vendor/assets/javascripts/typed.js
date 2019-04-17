34170c6378097bd1b60745bdbd2e26fa2e281c8e
cefaijustin <cefaijustin@gmail.com>
2019-04-16 18:59:14 -0700

    contact page remodel

--- vendor/assets/javascripts/typed.js
+++ vendor/assets/javascripts/typed.js
@@ -1,17 +1,14 @@
-import Typed from 'typed.js';
+import Typed from "typed.js";
 
 var options = {
   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
   typeSpeed: 100,
-  callback: function () {
-                $(".typed-cursor").hide();
-            }
-            onComplete: function(self) {  
-  		$('a[href="#web-apps"]').click(scrollToHash) 
-		}
-
-}
-
+  callback: function() {
+    $(".typed-cursor").hide();
+  },
+  onComplete: function(self) {
+    $('a[href="#web-apps"]').click(scrollToHash);
+  }
+};
 
 var typed = new Typed(".element", options);
-
