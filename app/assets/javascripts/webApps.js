017402e576dd09d55cc8c9ff70ed8d8fedf5a555
cefaijustin <cefaijustin@gmail.com>
2019-04-21 18:49:50 -0700

    progress bar block changed to window

--- app/assets/javascripts/webApps.js
+++ app/assets/javascripts/webApps.js
@@ -1,102 +1,104 @@
-jQuery(document).ready(function() {
-  jQuery(document).on("scroll", function() {
-    if (jQuery("html,body").scrollTop() > jQuery(".container").height()) {
-      jQuery(".progress-bar").each(function() {
-        jQuery(this)
-          .find(".progress-content")
-          .animate(
-            {
-              width: jQuery(this).attr("data-percentage")
-            },
-            2000
-          );
+document.addEventListener("turbolinks:load", function() {
+  $(document).ready(function() {
+    $(window).on("scroll", function() {
+      if ($("html,body").scrollTop() > $(".container").height()) {
+        $(".progress-bar").each(function() {
+          $(this)
+            .find(".progress-content")
+            .animate(
+              {
+                width: $(this).attr("data-percentage")
+              },
+              2000
+            );
 
-        jQuery(this)
-          .find(".progress-number-mark")
-          .animate(
-            {
-              left: jQuery(this).attr("data-percentage")
-            },
-            {
-              duration: 1500,
-              step: function(now, fx) {
-                var data = Math.round(now);
-                jQuery(this)
-                  .find(".percent")
-                  .html(data + "%");
+          $(this)
+            .find(".progress-number-mark")
+            .animate(
+              {
+                left: $(this).attr("data-percentage")
+              },
+              {
+                duration: 1500,
+                step: function(now, fx) {
+                  var data = Math.round(now);
+                  $(this)
+                    .find(".percent")
+                    .html(data + "%");
+                }
               }
-            }
-          );
-      });
+            );
+        });
+      }
+    });
+  });
+
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    if (sT >= 800 && sT <= 4500) {
+      $(".circle").css({ opacity: "1" });
+    } else {
+      $(".circle").css({ opacity: "0" });
     }
   });
-});
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  if (sT >= 800 && sT <= 4500) {
-    $(".circle").css({ opacity: "1" });
-  } else {
-    $(".circle").css({ opacity: "0" });
-  }
-});
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
+    var tag = $(".outer-box-l-1");
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  var sB = sT + $(window).height();
-  var tag = $(".outer-box-l-1");
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
 
-  if ($(tag).position().top < sB) {
-    $(tag).addClass("visible");
-  } else {
-    $(tag).removeClass("visible");
-  }
-});
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
+    var tag = $(".outer-box-r-1");
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  var sB = sT + $(window).height();
-  var tag = $(".outer-box-r-1");
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
 
-  if ($(tag).position().top < sB) {
-    $(tag).addClass("visible");
-  } else {
-    $(tag).removeClass("visible");
-  }
-});
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
+    var tag = $(".outer-box-l-2");
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  var sB = sT + $(window).height();
-  var tag = $(".outer-box-l-2");
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
 
-  if ($(tag).position().top < sB) {
-    $(tag).addClass("visible");
-  } else {
-    $(tag).removeClass("visible");
-  }
-});
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
+    var tag = $(".outer-box-r-2");
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  var sB = sT + $(window).height();
-  var tag = $(".outer-box-r-2");
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
 
-  if ($(tag).position().top < sB) {
-    $(tag).addClass("visible");
-  } else {
-    $(tag).removeClass("visible");
-  }
-});
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
+    var tag = $(".container");
 
-$(window).scroll(function() {
-  var sT = $(this).scrollTop();
-  var sB = sT + $(window).height();
-  var tag = $(".container");
-
-  if ($(tag).position().top < sB) {
-    $(tag).addClass("visible");
-  } else {
-    $(tag).removeClass("visible");
-  }
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
 });
