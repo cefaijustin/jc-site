f6e6cd315647cbdc45cf9280ba6263a8903e7d5b
Justin Cefai <justincefai@Justins-MacBook-Pro.local>
2019-05-05 16:06:16 -0700

    sass video added and borders removed

--- app/assets/javascripts/webApps.js
+++ app/assets/javascripts/webApps.js
@@ -45,6 +45,18 @@ document.addEventListener("turbolinks:load", function() {
   $(window).scroll(function() {
     var sT = $(this).scrollTop();
     var sB = sT + $(window).height();
+    var tag = $(".outer-box-video");
+
+    if ($(tag).position().top < sB) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  });
+
+  $(window).scroll(function() {
+    var sT = $(this).scrollTop();
+    var sB = sT + $(window).height();
     var tag = $(".outer-box-l-1");
 
     if ($(tag).position().top < sB) {
