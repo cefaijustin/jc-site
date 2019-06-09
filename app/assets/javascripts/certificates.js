dc160ebe9ef612499de01c2a78ffac80754c713e
Justin Cefai <justincefai@Justins-MacBook-Pro.local>
2019-06-08 17:02:43 -0700

    certificate enlarge action added

--- app/assets/javascripts/certificates.js
+++ app/assets/javascripts/certificates.js
@@ -34,4 +34,84 @@ document.addEventListener("turbolinks:load", function() {
       $(tag).removeClass("visible");
     }
   });
+
+  // firehose certificate below
+
+  $(document).ready(function() {
+    var button = document.getElementById("cert-fh-bg");
+
+    function cover() {
+      $(".cert-fh-bg").addClass("visible");
+    }
+    button.addEventListener("click", cover);
+  });
+
+  $(document).ready(function() {
+    var button = document.getElementById("close-bg-cert");
+
+    function uncover() {
+      $(".cert-fh-bg").removeClass("visible");
+    }
+    button.addEventListener("click", uncover);
+  });
+
+  // javascript certificate below
+
+  $(document).ready(function() {
+    var button = document.getElementById("cert-js-bg");
+
+    function cover() {
+      $(".cert-js-bg").addClass("visible");
+    }
+    button.addEventListener("click", cover);
+  });
+
+  $(document).ready(function() {
+    var button = document.getElementById("close-bg-cert1");
+
+    function uncover() {
+      $(".cert-js-bg").removeClass("visible");
+    }
+    button.addEventListener("click", uncover);
+  });
+
+  // algorithms certificate below
+
+  $(document).ready(function() {
+    var button = document.getElementById("cert-al-bg");
+
+    function cover() {
+      $(".cert-al-bg").addClass("visible");
+    }
+    button.addEventListener("click", cover);
+  });
+
+  $(document).ready(function() {
+    var button = document.getElementById("close-bg-cert2");
+
+    function uncover() {
+      $(".cert-al-bg").removeClass("visible");
+    }
+    button.addEventListener("click", uncover);
+  });
+
+  // shopify certificate below
+
+  $(document).ready(function() {
+    var button = document.getElementById("cert-sh-bg");
+
+    function cover() {
+      $(".cert-sh-bg").addClass("visible");
+    }
+    button.addEventListener("click", cover);
+  });
+
+  $(document).ready(function() {
+    var button = document.getElementById("close-bg-cert3");
+
+    function uncover() {
+      $(".cert-sh-bg").removeClass("visible");
+    }
+    button.addEventListener("click", uncover);
+  });
 });
